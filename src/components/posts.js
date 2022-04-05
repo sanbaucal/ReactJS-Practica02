6fb43e6e7a6b3cf9a04ac9df9d8926adaee97a97
SANDRO BAUTISTA CALDERON <sanbaucal_1916@hotmail.com>
2022-04-04 21:46:10 -0500

    Practica 02

--- src/components/posts.js
+++ src/components/posts.js
@@ -1,49 +1,46 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect } from "react";
 import moment from "moment";
 import Post from "./post";
 
-function Posts(){
-  const [data, setData] = useState([]);
-
-  const getData = () => {
-    fetch("episodes.json", {
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-    })
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (myJson) {
-        setData(myJson.episodes);
-      });
-  };
+function PostList({ view_active, posts }) {
+  const [spinner, setSpinner] = useState(true);
 
   useEffect(() => {
-    getData();
+    setTimeout(() => setSpinner(false), 3000);
   }, []);
 
-  return (
-    <div className="row">
-      {data &&
-        data.length > 0 &&
-        data.map((episode, i) => {
-          return (
-            <div className="col-xs-12 col-sm-6 col-lg-4 col-xl-3 mb-3" key={i}>
-              <Post
-                autor={episode.name}
-                description={episode.summary.slice(20)}
-                img={episode.image.medium}
-                fecha={moment(episode.airdate).fromNow()}
-                likes={0}
-                coments={0}
-              />
-            </div>
-          );
-        })}
+  return spinner ? (
+    <div className="d-flex justify-content-center mt-4">
+      <strong>Loading... </strong>
+      <div className="spinner-grow" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
     </div>
+  ) : (
+    view_active && !spinner && (
+      <div className="row">
+        {posts &&
+          posts.length > 0 &&
+          posts.map((episode, i) => {
+            return (
+              <div
+                className="col-xs-12 col-sm-6 col-lg-4 col-xl-3 mb-3"
+                key={i}
+              >
+                <Post
+                  autor={episode.name}
+                  text={episode.summary.slice(0, 50)}
+                  image={episode.image.medium}
+                  createAt={moment(episode.airdate).fromNow()}
+                  likes={0}
+                  comments={0}
+                />
+              </div>
+            );
+          })}
+      </div>
+    )
   );
 };
 
-export default Posts;
+export default PostList;
